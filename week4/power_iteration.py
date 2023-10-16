03c3fddb93e2b656ee287b8ed5119c8ea288c6a3
featztex <romaem3019@gmail.com>
2023-10-16 14:24:37 +0300

    finished

--- week4/power_iteration.py
+++ week4/power_iteration.py
@@ -11,4 +11,9 @@ def get_dominant_eigenvalue_and_eigenvector(data, num_steps):
     """
     ### YOUR CODE HERE
 
-    return 
+    n = data.shape[0]
+    r = np.random.uniform(low=-10, high=10, size=n)
+    for i in range(num_steps):
+        r = (data @ r) / np.linalg.norm(data @ r, ord=2)
+        mu = r.T @ (data @ r) / (r.T @ r)
+    return float(mu), r
